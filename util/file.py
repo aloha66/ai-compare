2f383dc6f317f21268f04def6724a70c150eda22
aloha66 <lzcbruce@live.com>
2024-08-09 07:10:10 +0800

    chore: update file

--- util/file.py
+++ util/file.py
@@ -5,11 +5,9 @@ def get_file_paths(data_folder):
     file_paths = []
     for root, dirs, files in os.walk(data_folder):
         for file in files:
-            file_path = os.path.join(root, file)
-            file_paths.append(file_path)
-    return file_paths
+            yield os.path.join(root, file)
 
 
-def load_file(path):
-    with open(path, encoding="utf-8") as f:
+def load_text(path):
+    with open(path,encoding="utf-8") as f:
         return f.read()
